03db955e21b5620f090e5b01f84fb6d0cbf58422
Dirane Ngousse <enaryd123@gmail.com>
2026-04-22 21:46:46 +0100

    update

---
+++ pos_branding_override/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "POS Branding Override",
+    "version": "1.0",
+    "depends": ["point_of_sale"],
+    "assets": {
+        "point_of_sale.assets": [
+            "pos_branding_override/static/src/xml/branding.xml",
+        ],
+    },
+    "installable": True,
+}
---
+++ web_company_title/static/src/js/web_company_title.js
@@ -0,0 +1,2 @@
+
+type C:\Users\Alwenas lingerie\.ssh\id_rsa.pub | ssh root@72.62.1.213 "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
